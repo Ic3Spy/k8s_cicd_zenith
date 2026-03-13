33dee503a19b5794d53c7f2e1f5fab57ee0b47d7
Ic3Spy <rhenz11sulit@gmail.com>
2026-03-13 08:59:38 +0800

    Vite fixes

--- client/react-testing-demo/vite.config.js
+++ client/react-testing-demo/vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/setupTests.js',
+    include: ['src/**/*.test.{js,jsx,ts,tsx}'] // Adjust the pattern to match your test files
   },
 })
